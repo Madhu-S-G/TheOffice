0ba8a105f2914d3e5d3d2f536efc2e0e13190e85
Madhu-S-G <madhusg.a8@gmail.com>
2024-04-19 17:48:52 -0700

    ccccccomit

--- com.healthkart.www/src/test/java/trailanderror/CheckTest.java
+++ com.healthkart.www/src/test/java/trailanderror/CheckTest.java
@@ -33,8 +33,8 @@ public class CheckTest {
 	@Test
 	public void strikers()
 	{
-		
-		System.out.println(" hi good evening guys!!!!!! ");
+		System.out.println("giiiii ");
+		System.out.println(" hi good evening  ");
 	}
 	
 	
